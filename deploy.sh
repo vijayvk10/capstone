2046cc842af257345519a9f29464f5cbaf74339a
vijayvk10 <126579351+vijayvk10@users.noreply.github.com>
2025-03-20 14:45:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u vijayvk10 -p dckr_pat_LCZREW7gY07F-j98gkF3n0M3YdU
+    docker tag test vijayvk10/dev
+    docker push vijayvk10/dev
+
     
